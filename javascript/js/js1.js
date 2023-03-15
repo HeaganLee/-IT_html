c8c4a6d66ef4a1a1a0b3b7576505091faccab302
HeaganLee <ekzmfkdl12@gmail.com>
2023-03-15 09:08:24 +0900

    20203-03-02 ~ 2023-03-15 css html

---
+++ javascript/js/js1.js
@@ -0,0 +1,15 @@
+// alert("알림창을 확인해 주세요");
+// alert("두번째 알림창");
+// alert(10+20);
+// console.log("println");
+// let flag = confirm("선택해주세요");
+// console.log(flag);
+// let name2 = prompt("이름을 입력해주세요");
+// console.log(name2);
+
+let test;
+console.log(test);
+
+let num1 = Number(prompt("첫번째 숫자 입력"));
+let num2 = parseInt(prompt("두번쨰 숫자 입력"));
+console.log(num1 + num2)
